ac67eda5013b3fbb46b4d6cbc76b75faaef5312a
jabbour dandan <111487226+jabbourdan@users.noreply.github.com>
2022-11-20 21:14:31 +0200

    Add mongo_conn

--- mongoDB.py
+++ mongoDB.py
@@ -9,3 +9,11 @@ class mongoDB():
         self.db = self.mongo_conn(ip,port)
         self.database 
         self.fs = gridfs.GridFS(self.db)
+        
+    def mongo_conn(self,ip ,port):
+        try:
+            conn = MongoClient(ip ,port)
+            self.database = conn["movies"]
+            return self.database
+        except Exception as e:
+            print("error in mongoDB connection ")
